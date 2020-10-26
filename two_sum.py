e4cb7aa8d216e683f0a11b5912854f6118f882de
Brittany Jones <brittany.r.jones@hotmail.com>
2020-10-26 10:50:59 -0700

    Update two_sum.py
    more readable

--- two_sum.py
+++ two_sum.py
@@ -1,14 +1,19 @@
-def two_sum(nums, target):
-    sums = []
-    num_hash = {}
-
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    inmap = {}
     for i in range(len(nums)):
-        sum_pair_number = target - nums[i]
-        if sum_pair_number in num_hash:
-            print(nums[i],sum_pair_number)
-        num_hash[nums[i]] = nums[i]
+        num = nums[i]
+        pair = target - num
+        if pair in inmap:
+            return [inmap[pair], i]
+        index_map[num] = i
+    return None
+ 
+nums = [1,4,8,2,2,9,16]
+target = 11
 
-nums = [4, 5, 1, 8]
-target = 9    
-  
-print(two_sum(nums, target))
+print(twoSum(nums, target))
